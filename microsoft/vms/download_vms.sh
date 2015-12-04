6c4a4441f4848f81e7accdbc308d2a9e642eceab
Kurtis Miller <kumiller@atlassian.com>
2015-12-03 23:53:07 -0600

    VM download shell script.

---
+++ microsoft/vms/download_vms.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://az792536.vo.msecnd.net/vms/VMBuild_20150916/Vagrant/IE11/IE11.Win7.Vagrant.zip
+wget https://az792536.vo.msecnd.net/vms/VMBuild_20150916/Vagrant/IE11/IE11.Win81.Vagrant.zip
+wget https://az792536.vo.msecnd.net/vms/VMBuild_20150916/Vagrant/MsEdge/MsEdge.Win10.Vagrant.zip
+
+# wget https://az792536.vo.msecnd.net/vms/VMBuild_20150916/Vagrant/IE10/IE10.Win8.Vagrant.zip
